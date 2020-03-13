015181d6942ec83cdd16cea80393d04c89e0d7db
ohhoney1 <1269075501@qq.com>
2020-03-13 15:28:38 +0800

    opt: optimize download-filt-by-fetch function

--- src/download-file-by-fetch.ts
+++ src/download-file-by-fetch.ts
@@ -1,19 +1,41 @@
 /**
+ * 从响应头 content-disposition 里获取文件名
+ * @param res fetch 的响应
+ * @param url 文件流信息
+ */
+function _getFileName(res: Response, url: string): string {
+  const dispositionInfo = res.headers.get('content-disposition')
+  let filename = url
+  if (dispositionInfo) {
+    try {
+      filename = decodeURI(dispositionInfo.split('filename=')[1])
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    }
+  }
+  return filename
+}
+
+/**
  * 使用 fetch 请求下载文件
  * @param addr 下载地址
  * @param options fetch 的配置信息
  * @param successCallback 接口成功返回的回调
  * @param failCallback 接口失败的回调
  * @param finishCallback 下载完成的回调
+ * @param context fetch 函数，用来覆盖原生的 fetch
  */
 export default function downloadFileByFetch(
   addr: string,
   options?: RequestInit,
   successCallback?: () => void,
   failCallback?: (err?: any) => void,
-  finishCallback?: () => void
-) {
-  fetch(addr, options)
+  finishCallback?: () => void,
+  context?: (input: RequestInfo, init?: RequestInit) => Promise<Response>
+): void {
+  const _fetch = context || fetch
+  _fetch(addr, options)
     .then(res => {
       if (successCallback) successCallback()
       res.blob().then(blob => {
@@ -26,14 +48,11 @@ export default function downloadFileByFetch(
           // 兼容ie10
           navigator.msSaveBlob(blob)
         } else {
-          const dispositionInfo = res.headers.get('content-disposition')
           const a = document.createElement('a')
           const url = window.URL.createObjectURL(blob)
           document.body.appendChild(a)
           a.href = url
-          a.download = dispositionInfo
-            ? decodeURI(dispositionInfo.split('filename=')[1])
-            : url
+          a.download = _getFileName(res, url)
           a.click()
           a.remove()
           window.URL.revokeObjectURL(url)
