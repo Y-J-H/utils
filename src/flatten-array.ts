ffce80678d0977daee3b22897b0721ede8b0d79a
ohhoney1 <1269075501@qq.com>
2020-05-09 15:06:14 +0800

    lint: lint flatten-array

--- src/flatten-array.ts
+++ src/flatten-array.ts
@@ -2,6 +2,9 @@
  * 数组扁平化
  * @param arr 原始数组
  */
-export default function flattenArray<T = any> (arr: T[]): T[] | T {
-  return arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flattenArray(val) : val), ([] as T[]))
+export default function flattenArray<T = any>(arr: T[]): T[] | T {
+  return arr.reduce(
+    (acc, val) => acc.concat(Array.isArray(val) ? flattenArray(val) : val),
+    [] as T[]
+  )
 }
